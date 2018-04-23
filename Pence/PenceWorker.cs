640c5748f527655c1980186144a77c4b55b1d10b
KateKoss <777koska@gmail.com>
2018-04-23 22:58:48 +0300

    Initial commit.

---
+++ Pence/PenceWorker.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pence
+{
+    class PenceWorker
+    {
+        private int[] penceArray;
+        private  int amountOfPennyForExchange;
+        public PenceWorker(int amountOfPennyForExchange, int[] penceArray)
+        {
+            this.amountOfPennyForExchange = amountOfPennyForExchange;
+            this.penceArray = penceArray;
+        }
+        public int findCombinationsOfExchange()
+        {
+            return countingCombinations(amountOfPennyForExchange, penceArray.Length);
+        }
+        public int countingCombinations(int amount, int coinType)
+        {
+            if (amount == 0)
+                return 1;
+            if (amount < 0 || coinType == 0)
+                return 0;
+            else
+                return countingCombinations(amount, coinType - 1) + countingCombinations(amount - coinValue(coinType), coinType);
+        }
+
+        public int coinValue(int coinType)  // 1 < coinType < penceArray.Length
+        {
+            return penceArray[coinType - 1];
+        }
+    }
+}
---
+++ Pence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] penceArray = new int[] { 200, 100, 50, 20, 10, 5, 2, 1 };
+            int amountOfPennyForExchange = 200;
+            PenceWorker penceWorker = new PenceWorker(amountOfPennyForExchange, penceArray);
+            Console.WriteLine("Combinations of exchange: " + penceWorker.findCombinationsOfExchange());
+            Console.ReadKey();
+        }
+    }
+}
